bc257a7dd5d311ebeb1ff12202ea412f8a1bf992
igorb22 <igorb22@live.com>
2020-04-13 23:27:58 -0300

    atualizacoes

---
+++ Codigo/Service/MunicipioService.cs
@@ -0,0 +1,55 @@
+﻿using Model;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Service.Interface
+{
+    public class MunicipioService : IMunicipioService
+    {
+        private readonly monitorasusContext _context;
+
+        public MunicipioService(monitorasusContext context)
+        {
+            _context = context;
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<MunicipioModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public MunicipioModel GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MunicipioModel GetByName(string name)
+         => _context.Municipio
+                .Where(municipioModel => municipioModel.Nome.ToUpper().Equals(name.ToUpper()))
+                .Select(municipio => new MunicipioModel
+                {
+                    Id = municipio.Id,
+                    Nome = municipio.Nome,
+                    Uf = municipio.Uf,
+                    Codigo = municipio.Codigo
+                }).FirstOrDefault();
+
+        public bool Insert(MunicipioModel municipioModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(MunicipioModel municipioModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Codigo/Service/SituacaoVirusBacteriaService.cs
@@ -0,0 +1,59 @@
+﻿using Model;
+using Persistence;
+using Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Service
+{
+    public class SituacaoVirusBacteriaService : ISituacaoVirusBacteriaService
+    {
+
+        private readonly monitorasusContext _context;
+
+        public SituacaoVirusBacteriaService(monitorasusContext context)
+        {
+            _context = context;
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<SituacaoPessoaVirusBacteriaModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public SituacaoPessoaVirusBacteriaModel GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SituacaoPessoaVirusBacteriaModel GetByIdPessoa(int idPessoa)
+       => _context.Situacaopessoavirusbacteria
+                .Where(situacaoPessoa => situacaoPessoa.Idpessoa == idPessoa)
+                .Select(situacao => new SituacaoPessoaVirusBacteriaModel
+                {
+                    
+                    Idpessoa = situacao.Idpessoa,
+                    IdVirusBacteria = situacao.IdVirusBacteria,
+                    UltimaSituacaoSaude = situacao.UltimaSituacaoSaude
+
+                }).FirstOrDefault();
+
+
+        public bool Insert(SituacaoPessoaVirusBacteriaModel situacaoPessoaVirusBacteriaModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(SituacaoPessoaVirusBacteriaModel situacaoPessoaVirusBacteriaModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
