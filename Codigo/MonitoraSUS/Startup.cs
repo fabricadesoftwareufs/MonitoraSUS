a4b7a5ad1e614ac763764963827e41d5da1f3853
igorb22 <igorb22@live.com>
2020-04-22 17:49:12 -0300

    correções

--- Codigo/MonitoraSUS/Startup.cs
+++ Codigo/MonitoraSUS/Startup.cs
@@ -61,17 +61,20 @@ namespace MonitoraSUS
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-           /*
-            * if (env.IsDevelopment())
-            * {
-            *    app.UseDeveloperExceptionPage();
-            * }
-            * else{}
-            */
+            /*
+             * if (env.IsDevelopment())
+             * {
+             *    app.UseDeveloperExceptionPage();
+             * }
+             * else
+             * {
+             *   app.UseHsts();
+             * }
+             */
 
             app.UseExceptionHandler("/Error/500");
             app.UseStatusCodePagesWithReExecute("/Error/{0}");
-            app.UseHsts();
+
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
