16b44b2d7ff7d7c5ce1d8dca7ac36fbbb8b89f65
marcosdosea <marcosdosea@yahoo.com.br>
2020-04-26 10:59:14 -0300

    Correções de mensagens de erros no cadastro. cpf como campo único no modelo de dados apenas um exame pode ser cadastro para o paciente no cadastro.

--- Codigo/Model/PessoaModel.cs
+++ Codigo/Model/PessoaModel.cs
@@ -18,10 +18,12 @@ namespace Model
         [Display(Name = "Sexo")]
         public string Sexo { get; set; }
         public string Cep { get; set; }
-        public string Rua { get; set; }
+		public string Rua { get; set; }
         public string Bairro { get; set; }
-        public string Cidade { get; set; }
-        public string Estado { get; set; }
+		[Required]
+		public string Cidade { get; set; }
+		[Required]
+		public string Estado { get; set; }
         public string Numero { get; set; }
         public string Complemento { get; set; }
         public string Latitude { get; set; }
