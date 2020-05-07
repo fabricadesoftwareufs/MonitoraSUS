a290a703422a25c483cd91065cfd7be310f95f91
Gabriel Santana <gabriel.sistemasjr@gmail.com>
2020-05-07 16:09:05 -0300

    Configuração do captcha na view de criação de agente/secreraio. Configuração do case de criação na controller. Criação do metodo de validação dos dados recebidos e classe para desserializar o objeto recebido.

---
+++ Codigo/Model/AuxModel/RecaptchaModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.AuxModel
+{
+    public class RecaptchaModel
+    {
+        public bool Success { get; set; }
+        public DateTime Chanllenge_ts { get; set; }
+        public string HostName { get; set; }
+        public float Score { get; set; }
+        public string Action { get; set; }
+        public Array ErrorCodes { get; set; }
+    }
+}
