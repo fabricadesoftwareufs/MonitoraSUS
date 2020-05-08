9c9d1f424c2094f57058b0a2712c54118af5829b
Abraao Alves <abraao1514@gmail.com>
2020-05-07 21:35:12 -0300

    mudanca de api

---
+++ Codigo/Model/AuxModel/ConfiguracaoNotificarModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.AuxModel
+{
+    public class ConfiguracaoNotificarModel
+    {
+        public int IdConfiguracaoNotificar { get; set; }
+        public bool HabilitadoSms { get; set; }
+        public bool HabilitadoWhatsapp { get; set; }
+        public string Sid { get; set; }
+        public string Token { get; set; }
+        public string NumeroSms { get; set; }
+        public string NumeroWhatsapp { get; set; }
+        public string MensagemPositivo { get; set; }
+        public string MensagemNegativo { get; set; }
+        public string MensagemImunizado { get; set; }
+        public string MensagemIndeterminado { get; set; }
+        public int? IdEstado { get; set; }
+        public int? IdMunicipio { get; set; }
+        public int? IdEmpresaExame { get; set; }
+    }
+}
---
+++ Codigo/Model/AuxModel/ResponseMultipleSMSModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.AuxModel
+{
+    public class ResponseMultipleSMSModel
+    {
+        public string Number { get; set; }
+        public string Id { get; set; }
+        public string Refer { get; set; }
+        public string Situacao { get; set; }
+        public string Descricao { get; set; }
+    }
+}
