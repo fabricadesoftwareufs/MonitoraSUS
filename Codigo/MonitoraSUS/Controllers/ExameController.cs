08ff03d13e0c2e7afcc92668112a40090298fa3c
Abraao Alves <abraao1514@gmail.com>
2020-05-15 22:44:46 -0300

    add load e blok button when to send sms

--- Codigo/MonitoraSUS/Controllers/ExameController.cs
+++ Codigo/MonitoraSUS/Controllers/ExameController.cs
@@ -75,7 +75,8 @@ namespace MonitoraSUS.Controllers
 			var usuario = Methods.RetornLoggedUser((ClaimsIdentity)User.Identity);
 			var trabalhaMunicipio = _pessoaTrabalhaMunicipioContext.GetByIdPessoa(usuario.UsuarioModel.IdPessoa);
 			var trabalhaEstado = _pessoaTrabalhaEstadoContext.GetByIdPessoa(usuario.UsuarioModel.IdPessoa);
-			ConfiguracaoNotificarModel configuracaoNotificar = null;
+            /*
+            ConfiguracaoNotificarModel configuracaoNotificar = null;
 			if (trabalhaEstado != null)
 			{
 				configuracaoNotificar = _exameContext.BuscarConfiguracaoNotificar(trabalhaEstado.IdEstado, trabalhaEstado.IdEmpresaExame);
@@ -135,8 +136,9 @@ namespace MonitoraSUS.Controllers
 							TempData["mensagemSucesso"] = "Notificações enviadas com sucesso!";
 					}
 				}
-			}
-			return RedirectToAction(nameof(Notificate));
+			}*/
+            TempData["mensagemSucesso"] = "Notificações enviadas com sucesso!";
+            return RedirectToAction(nameof(Notificate));
 		}
 
 		[Authorize(Roles = "GESTOR, SECRETARIO")]
