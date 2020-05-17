709519e176e1e0d1a5abf4cec15e2997461cc150
Abraao Alves <abraao1514@gmail.com>
2020-05-16 23:08:14 -0300

    trocando modal por select e add confirmacao Fix #108

--- Codigo/MonitoraSUS/wwwroot/assets/js/agente-secretario.js
+++ Codigo/MonitoraSUS/wwwroot/assets/js/agente-secretario.js
@@ -1,10 +1,10 @@
 ﻿$(document).ready(function () {
-        var msg = document.getElementById("msg");
-    if(msg != null)
+    var msg = document.getElementById("msg");
+    if (msg != null)
         msg.click();
 })
 
-function swtAlert(type,title,message){
+function swtAlert(type, title, message) {
     Swal.fire({
         icon: type,
         title: title,
@@ -12,60 +12,65 @@ function swtAlert(type,title,message){
     });
 }
 
-$('#empresaModal').on('show.bs.modal', function(e) {
-	let cpf = document.getElementById('input-cpf').value;
-	if (cpf == "") {
-		cpf = e.relatedTarget.dataset.cpf;
-	}
+$('#empresaModal').on('show.bs.modal', function (e) {
+    let cpf = document.getElementById('input-cpf').value;
+    if (cpf == "") {
+        cpf = e.relatedTarget.dataset.cpf;
+    }
     $(".modal-body #modal-cpf").text(cpf);
 });
 
 $('#agentModal').on('show.bs.modal', function (e) {
-	var id = e.relatedTarget.dataset.id;
-	var nome = e.relatedTarget.dataset.nome;
-	var funcao = e.relatedTarget.dataset.funcao;
-	$(".modal-body #modal-id").text(id);
-	$(".modal-body #modal-nome").text(nome);
-	$(".modal-body #modal-funcao").text(funcao);
+    var id = e.relatedTarget.dataset.id;
+    var nome = e.relatedTarget.dataset.nome;
+    var funcao = e.relatedTarget.dataset.funcao;
+    $(".modal-body #modal-id").text(id);
+    $(".modal-body #modal-nome").text(nome);
+    $(".modal-body #modal-funcao").text(funcao);
 });
 
 $('#btnActivate').on('click', function () {
-	let url = "/AgenteSecretario/ExistePessoa";
-    let cpf = document.getElementById('input-cpf')
+    let url = "/AgenteSecretario/ExistePessoa";
+    let cpf = $('#input-cpf').val().replace(/[^0-9]/g, "");;
+    let idEmpresa = $('#selectEmpresa').val();
+    let action = $('#entidade').text();
 
-    $.post(url, {cpf: cpf.value }, function (data) {
+    $.post(url, { cpf: cpf }, function (data) {
         if (!data) {
-	        swtAlert('error', 'Falha', 'O CPF informado não está cadastrado no sistema.');
-        } else {
-            $("#empresaModal").modal('show');
+            swtAlert('error', 'Falha', 'O CPF informado não está cadastrado no sistema.');
+        } else if (idEmpresa == null) {
+            swtAlert('error', 'Falha', 'Por favor, selecione uma empresa.');
+        }
+        else {
+            Swal.fire({
+                title: 'Confirmar associação?',
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Sim ',
+                cancelButtonText: 'Não'
+            }).then(function (result) {
+                if (result.value) {
+
+                    let url = "/AgenteSecretario/Activate";
+                    if (action == 'Agente')
+                        window.location.href = url + '/Agente/' + cpf + "/" + idEmpresa;
+                    else if (action == 'Gestor')
+                        window.location.href = url + '/Gestor/' + cpf + "/" + idEmpresa;
+                }
+            });
         }
     });
 });
-
-function associar(idEmpresa, action) {
-	var cpf = document.getElementById('input-cpf').value;
-	if (cpf == "") {
-		cpf = $("#modal-cpf").text();
-		cpf = cpf.replace(/[^0-9]/g, "");
-	}
-	//var url = '@Url.Action("Activate", "AgenteSecretario")';
-	let url = "/AgenteSecretario/Activate";
-	if (action == 'Agente')
-		window.location.href = url + '/Agente/' + cpf + "/" + idEmpresa;
-	else if (action == 'Gestor')
-		window.location.href = url + '/Gestor/' + cpf + "/" + idEmpresa;
-}
-
 
 function actionDel() {
     var action = $("#modal-funcao").text();
     var idPessoa = $("#modal-id").text();
 
-	//var url = '@Url.Action("Delete", "AgenteSecretario")';
-	let url = "/AgenteSecretario/Delete";
+    //var url = '@Url.Action("Delete", "AgenteSecretario")';
+    let url = "/AgenteSecretario/Delete";
 
     if (action == 'Agente')
         window.location.href = url + '/Agente/' + idPessoa;
-    else if(action == 'Gestor')
+    else if (action == 'Gestor')
         window.location.href = url + '/Gestor/' + idPessoa;
 }
