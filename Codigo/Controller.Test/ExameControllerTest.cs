8ac362125afd01cd3fc5574cca573b0f60900aa3
Abraao Alves <abraao1514@gmail.com>
2020-07-08 16:23:30 -0300

    add test controller exame

--- Codigo/Controller.Test/ExameControllerTest.cs
+++ Codigo/Controller.Test/ExameControllerTest.cs
@@ -1,4 +1,6 @@
-﻿using Xunit;
+﻿using Model;
+using System.Collections.Generic;
+using Xunit;
 
 namespace Controller.Test
 {
@@ -7,7 +9,50 @@ namespace Controller.Test
         [Fact(DisplayName = "Adiciona um Exame com sucesso e Retorna o redirect de quando o sts do modelo é válido")]
         public void AddExameComSucesso()
         {
-
+            
         }
+
+        private List<ExameModel> GetExames()
+        => new List<ExameModel>
+        {
+            new ExameModel
+            {
+                IgG = "N",
+                IgGIgM = "N",
+                IgM = "N",
+                Pcr = "N",
+                IdEstado = 1,
+                IdMunicipio = 1,
+                IdEmpresaSaude = 1,
+                IdNotificacao = null,
+                CodigoColeta = "ABC",
+                StatusNotificacao = "N",
+            }, new ExameModel
+            {
+                IgG = "S",
+                IgGIgM = "S",
+                IgM = "S",
+                Pcr = "S",
+                IdEstado = 4,
+                IdMunicipio = 4,
+                IdEmpresaSaude = 4,
+                IdNotificacao = null,
+                CodigoColeta = "ABCS",
+                StatusNotificacao = "N",
+            },
+            new ExameModel
+            {
+                IgG = "S",
+                IgGIgM = "S",
+                IgM = "S",
+                Pcr = "N",
+                IdEstado = 2,
+                IdMunicipio = 2,
+                IdEmpresaSaude = 2,
+                IdNotificacao = null,
+                CodigoColeta = "AB",
+                StatusNotificacao = "N",
+            }
+        };
     }
 }
